9e64c68d52e393bde3205112913986ec7954d6f5
Ingeborg Wenger <ingeborg.wenger@itm.uni-stuttgart.de>
2026-02-17 14:25:01 +0100

    simulation run

---
+++ run.py
@@ -0,0 +1,67 @@
+import logging
+import multiprocessing
+import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from robots.build_robot_swarm import build_random_robot_swarm, build_simulation_robot
+from robots.robot_swarm import WarehouseSwarm
+from robots.save_and_load_swarm import save_robot_swarm
+from robots.sensors.robot_simulation import Hera
+from warehouse_env.warehouse import Warehouse
+
+if __name__ == "__main__":
+
+    logger = multiprocessing.log_to_stderr()
+    logger.setLevel(logging.WARNING)
+
+    config = {
+        "n_runs": 150,
+        "n_min_robots": 7,
+        "n_max_robots": 17,
+        "ts_communicate": 1.2,
+        "ts_control": 0.2,
+        "max_vel": 0.4,
+        "wait_for_ts_communicate": True,
+    }
+
+    warehouse = Warehouse(
+        graphml_path="data/training_validation_data/warehouse/Warehouse_preprocessed.graphml"
+    )
+
+    for run in range(20, config["n_runs"]):
+
+        print("##############################################################")
+        print(run)
+        print("##############################################################")
+        print("Run started at ", time.strftime("%H:%M:%S"))
+        np.random.seed(run)
+        n_robots = np.random.randint(config["n_min_robots"], config["n_max_robots"] + 1)
+
+        swarm, sensors = build_random_robot_swarm(
+            n_robots=n_robots,
+            build_robot_fn=build_simulation_robot,
+            simulation_type=Hera,
+            swarm_type=WarehouseSwarm,
+            ts_communicate=config["ts_communicate"],
+            ts_control=config["ts_control"],
+            max_vel=config["max_vel"],
+            wait_for_ts_communicate=config["wait_for_ts_communicate"],
+        )
+        swarm.start_run(warehouse, sensors)
+        swarm.run_swarm_task(max_tasks=int(n_robots * 3))
+        swarm.stop_run(sensors)
+
+        save_robot_swarm(
+            swarm,
+            f"data/training_validation_data/swarms_reruns/swarm{run}",
+        )
+        # swarm.visualization_module.animate_run(
+        #     folder_path=f"data/training_validation_data/run_animations/swarm{run}_rerun",
+        #     every_nth_img=10,
+        # )
+
+        # os.chdir(f"data/example_run_animation/swarm{run}")
+        # os.system("ffmpeg -i frame_%04d.png -vcodec mpeg4 -r 10 swarm.avi")
